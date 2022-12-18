f78c1fe8c6c7fccee935da3bb90feb7ed0a59f1b
jedrzejkozal <jedrzej.kozal@gmail.com>
2022-12-18 15:12:51 +0100

    update results

--- run0.sh
+++ run0.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # python main.py --method="naive" --interactive_logger=0 ----device="cuda:0" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="naive"
-python main.py --method="ewc" --interactive_logger=0 ----device="cuda:0" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="ewc"
-python main.py --method="agem" --interactive_logger=0 ----device="cuda:0" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="agem"
-python main.py --method="replay" --interactive_logger=0 ----device="cuda:0" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="replay"
+python main.py --method="ewc" --interactive_logger=0 --device="cuda:0" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="ewc"
+python main.py --method="agem" --interactive_logger=0 --device="cuda:0" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="agem"
+python main.py --method="replay" --interactive_logger=0 --device="cuda:0" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="replay"
 
--- run1.sh
+++ run1.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-python main.py --method="lwf" --interactive_logger=0 ----device="cuda:1" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="lwf"
-python main.py --method="mir" --interactive_logger=0 ----device="cuda:1" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="mir"
-python main.py --method="icarl" --interactive_logger=0 ----device="cuda:1" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="icarl"
+python main.py --method="lwf" --interactive_logger=0 --device="cuda:1" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="lwf"
+python main.py --method="mir" --interactive_logger=0 --device="cuda:1" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="mir"
+python main.py --method="icarl" --interactive_logger=0 --device="cuda:1" --n_experiences=20 --adversarial_attacks="same" --n_epochs=5 --run_name="icarl"
